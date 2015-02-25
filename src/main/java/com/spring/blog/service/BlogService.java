3e72125b53b09576803c021942ac0e277d5479d8
YIN CHAO <yin_chao@ymail.com>
2015-02-25 17:52:18 -0600

    reload each blog with @scheduled

--- src/main/java/com/spring/blog/service/BlogService.java
+++ src/main/java/com/spring/blog/service/BlogService.java
@@ -3,6 +3,7 @@ package com.spring.blog.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.security.access.method.P;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
@@ -30,7 +31,7 @@ public class BlogService {
 	@Autowired
 	private UserRepository userRepository;
 	
-	public void saveItem(Blog blog) {
+	public void saveItems(Blog blog) {
 		
 		try {
 			List<Item> items = rssService.getItems(blog.getUrl());
@@ -47,12 +48,20 @@ public class BlogService {
 			e.printStackTrace();
 		}
 	}
+	// 1 hour = 60 seconds * 60 minutes * 1000
+	@Scheduled(fixedDelay=3600000)
+	public void reloadBlogs() {
+		List<Blog> blogs = blogRepository.findAll();
+		for(Blog blog: blogs) {
+			saveItems(blog);
+		}
+	}
 	
 	public void save(Blog blog, String name) {
 		User user = userRepository.findByName(name);
 		blog.setUser(user);
 		blogRepository.save(blog);
-		saveItem(blog);
+		saveItems(blog);
 	}
 
 	@PreAuthorize("#blog.user.name == authentication.name or hasRole('ROLE_ADMIN')")
